3011b9379e8d8d7dfceeec714e88570e65dc8420
vamshi4220 <ud75281@gmail.com>
2020-10-31 10:16:42 +0530

    added last name

--- User_registration.sh
+++ User_registration.sh
@@ -10,3 +10,14 @@ then
 else
     echo "no"
 fi
+
+#UC2
+
+read -p "enter a surname " surname
+p2='^[A-Z]{1}[a-zA-Z]{3,15}$';
+if [[ $surname =~ $p2 ]]
+then
+    echo "yes"
+else
+    echo "no"
+fi
